82174d6fc915176f66bcaa64e4ad05978559c31b
Prekshya Dawadi <prekshya019@gmail.com>
2022-07-30 21:31:09 +0000

    code integration complete

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import image from './Images/bgimage1.jpg';
 import './HomePage.css'
+import kimage from './Images/kounsel-k.png'
 
 const HomePage = () => {
     let slogan = "A Safe Space"
@@ -17,6 +18,36 @@ const HomePage = () => {
                     </div>
                 </div>
             </div>
+
+        <div class="site-intro">
+            <div class="intro-kounsel">
+                <div class="text-area">
+                    <h1>What is Kounsel?</h1>
+                    <p>Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University.</p>
+                </div>
+                <div class="img-area">
+                    <img className="img-responsive img-fluid bgHImg" src={kimage} alt="bg-pic" />
+                </div>
+            </div>
+            <div class="intro-counselor">
+                <div class="img-area">
+                    <img className="img-responsive img-fluid bgHImg" src={kimage} alt="bg-pic" />
+                </div>
+                <div class="text-area">
+                    <h1>Meet the counselor</h1>
+                 <p>Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. </p>
+                </div>
+            </div>
+            <div class="intro-anonymous">
+                <div class="text-area">
+                    <h1>Your conversations are anonymous</h1>
+                    <p>Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. Kounsel is the unofficial counseling service of Kathmandu University. </p>
+                </div>
+                <div class="img-area">
+                    <img className="img-responsive img-fluid bgHImg" src={kimage} alt="bg-pic" />
+                </div>
+            </div>
+        </div>
         </>
     );
 }
