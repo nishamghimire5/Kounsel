83be243c9f467cc3e533bae7d233a44b19d2c995
Prekshya Dawadi <prekshya019@gmail.com>
2022-07-30 23:35:03 +0000

    added carousel but the image is not sliding ---tobe fixed

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,15 @@
 import image from './Images/bgimage1.jpg';
 import './HomePage.css'
 import kimage from './Images/kounsel-k.png'
+import slideImage1 from './Images/session.jpeg'
+import slideImage2 from './Images/stuffd.jpeg'
+import slideImage3 from './Images/123.jpeg'
 
 const HomePage = () => {
     let slogan = "A Safe Space"
     return (
         <>
-            <div className="box ">
+            {/* <div className="box ">
                 <img className="img-responsive img-fluid bgHImg" src={image} alt="bg-pic" />
                 <div className="img-title">
                     <div className="dropdown font-sans tracking-wider">
@@ -17,7 +20,33 @@ const HomePage = () => {
                         </button>
                     </div>
                 </div>
-            </div>
+            </div> */}
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+        <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={image} class="d-block w-100" alt={image} />
+    </div>
+    <div class="carousel-item">
+      <img src={image} class="d-block w-100" alt= {image}/>
+    </div>
+    <div class="carousel-item">
+      <img src={image} class="d-block w-100" alt= {image}/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
 
         <div class="site-intro">
             <div class="intro-kounsel">
