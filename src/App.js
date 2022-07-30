af5e000e8c030b14c5a20541bdcfd96160a941d3
Nisham Ghimire <nishamghimire5@gmail.com>
2022-07-30 04:01:52 -0700

    Footer in single page +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import TitleBar from './TitleBar'
 import HomePage from './HomePage'
+import FooterPage from './FooterPage'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div className="App">
       <TitleBar />
       <HomePage />
+      <FooterPage />
     </div>
   );
 }
