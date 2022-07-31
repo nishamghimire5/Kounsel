1deb0bdd4df16012c7ca73df746681e51f915e4f
Nisham Ghimire <nishamghimire5@gmail.com>
2022-07-31 22:26:50 +0545

    Routing introduced hooks left

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,16 @@
 import image from './Images/bgimage1.jpg';
 import './HomePage.css'
+import Cards from './Cards'
+import Testimonials from './Testimonials'
 
 const HomePage = () => {
     let slogan = "A Safe Space"
     return (
         <>
+            {/* background image in Homepage */}
             <div className="box ">
                 <img className="img-responsive img-fluid bgHImg" src={image} alt="bg-pic" />
+                {/* On the image text */}
                 <div className="img-title">
                     <div className="dropdown font-sans tracking-wider">
                         <h2>Kounsel</h2>
@@ -17,6 +21,10 @@ const HomePage = () => {
                     </div>
                 </div>
             </div>
+            {/* cards of what we can help with section*/}
+            <Cards />
+            {/* Testimonials section*/}
+            <Testimonials />
         </>
     );
 }
