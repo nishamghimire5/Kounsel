12dcfb8eb3926a87c76a8ff06e956fa55ff01828
Prekshya Dawadi <prekshya019@gmail.com>
2022-07-31 13:29:25 +0000

    Added the text on hover(cards), but the image position is messed up.

--- src/CardData.js
+++ src/CardData.js
@@ -4,22 +4,15 @@ import { Card } from "react-bootstrap";
 const CardData = (props) => {
     // let height = { "mix-height": "390px" };
     return (
-
         <div className="col-sm-12 col-md-6 col-lg-4 mb-4 " > <div className="card text-white card-has-bg click-col" >
-            <img className="card-img" src={props.imgLink} alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?" />
-            <div className="card-img-overlay d-flex flex-column">
-                <div className="card-body">
-                    <h4 className="card-title mt-0 "><a className="text-white" herf="/">{props.textInPic}</a></h4>
-                </div>
-                <div className="card-footer">
-                    <h3>{props.textInPic}</h3>
-
-                    {/* <div className="media">
-                        <p className="m-3" >hello</p>
-                    </div> */}
+                 <div className="card-body">
+                    <a href="#">
+                        <img className="card-img-overlay" src={props.imgLink} alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?" />
+                        <h4 className="banner-content"><a className="text-white" herf="/">{props.textInPic}</a></h4>
+                    </a>
                 </div>
             </div>
-        </div></div >
+        </div>
 
     )
 }
