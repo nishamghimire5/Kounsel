47fbbefb9d6eead09f934c0ce59821ea038136ca
Prekshya Dawadi <prekshya019@gmail.com>
2022-07-31 13:49:40 +0000

    Added the text on hover(cards), image fixed, hover to be fixed.

--- src/CardData.js
+++ src/CardData.js
@@ -5,15 +5,17 @@ const CardData = (props) => {
     // let height = { "mix-height": "390px" };
     return (
         <div className="col-sm-12 col-md-6 col-lg-4 mb-4 " > <div className="card text-white card-has-bg click-col" >
-                 <div className="card-body">
-                    <a href="#">
-                        <img className="card-img-overlay" src={props.imgLink} alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?" />
-                        <h4 className="banner-content"><a className="text-white" herf="/">{props.textInPic}</a></h4>
-                    </a>
+            <a href = "#">
+            <img className="card-img" src={props.imgLink} alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?" />
+            <div className="card-img-overlay d-flex flex-column">
+                <div className="card-body">
+                    <h4 className="card-title mt-0 "><a className="text-white" herf="/">{props.textInPic}</a></h4>
                 </div>
+                <div className="card-footer">
             </div>
-        </div>
-
+            </div>
+            </a>
+        </div></div>
     )
 }
 
