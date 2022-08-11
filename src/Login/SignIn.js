cf0675e229cbbad37c842fc7913abc75d4a5d45a
Prekshya Dawadi <prekshya019@gmail.com>
2022-08-11 15:06:13 +0000

    Login Signup form attempt 3

--- src/Login/SignIn.js
+++ src/Login/SignIn.js
@@ -3,55 +3,86 @@ import React,{useState} from "react"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import {Link} from 'react-router-dom'
-import '../styles/SignIn.css'
+import '../styles/SignUp.css'
 
 {/* <div className = "container"> */}
 
 export default function SignIn() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [faculty, setFaculty] = useState("");
+    const [regisNo, setRegistrationNumber] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     function validateForm(){
-        return email.length>0 && password.length>0;
+        return firstName.length>0 && lastName.length>0 && faculty.length>0 && regisNo.length>0 && email.length>0 && password.length>0;
     }
     function handleSubmit(event){
         event.preventDefault();
     }
     return (
-        // <> </> needed to add-in the js code in react
-        <>
-            {/* <> </> needed to add-in the js code in react */}
             <>
-                <h1 className="heading">SignIn</h1>
-                
-                <br />
-                <br />
-                <img width = "300" src="https://img.freepik.com/free-vector/notebook-concept-illustration_114360-387.jpg?w=826&t=st=1660200940~exp=1660201540~hmac=1c3efda040d782499db9b0fff61f26d623c6e3e9bc4ebafad2311078dfe62879"></img>
-                <Form class="form" onSubmit={handleSubmit}>
-                    <Form.Group controlId="email">
-                        <Form.Label className="">Email</Form.Label>
-                        <Form.Control className = "w-50" autoFocus type="email" value={email}
-                            onChange={(e) => setEmail(e.target.value)} />
-                    </Form.Group>
+            <div class="bg-img">
+            <div class="content">
+            <header>Sign Up Form</header>
 
-                    <br />
-                    <br />
-                    <Form.Group controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control className = "w-50" type="password" value={password}
-                            onChange={(e) => setPassword(e.target.value)} />
-                    </Form.Group>
-                    <br></br>
-                    <Button block size="lg" type="submit" disabled={!validateForm()}>
-                        SignIn
-                    </Button>
-                </Form>
-            </><br />
-            <span>
-                Don't have an account?  
-                <Link to="/signup"> Sign Up</Link>
-                <br /><br />
-                <Link to="/">Go back </Link>
-            </span>
-            </>
+            <form action="#">
+
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input type="text" required placeholder="First Name" />
+            </div>
+
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input type="text" required placeholder="Last Name" />
+            </div>
+            
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input type="text" required placeholder="Faculty" />
+            </div>
+            
+            <div class="field">
+                <span class="fa fa-user"></span>
+                <input type="text" required placeholder="Registration Number" />
+            </div>
+
+               <div class="field">
+                  <span class="fa fa-user"></span>
+                  <input type="text" required placeholder="Email or Phone" />
+               </div>
+               <div class="field space">
+                  <span class="fa fa-lock"></span>
+                  <input type="password" class="pass-key" required placeholder="Password" />
+                  <span class="show">SHOW</span>
+               </div>
+               <div class="pass">
+                  <a href="#">Forgot Password?</a>
+               </div>
+               <div class="field">
+                  <input type="submit" value="SIGN UP" />
+               </div>
+            </form>
+            <div class="login">
+               Or signup with
+            </div>
+            <div class="links">
+               <div class="facebook">
+                  <i class="fab fa-facebook-f"><span>Facebook</span></i>
+               </div>
+               <div class="instagram">
+                  <i class="fab fa-instagram"><span>Instagram</span></i>
+               </div>
+            </div>
+            <div class="signup">
+               Already have an account?
+               <a href="#">Login</a>
+            </div>
+         </div>
+      </div>
+    <div>
+    </div>
+    </>
     );
 }
