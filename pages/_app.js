6d4f3e03cf65466a53bed63d3f40e60cd6ca1ce8
aayush125 <aayushbhatt54@gmail.com>
2022-09-08 17:16:19 +0545

    next version

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import '../styles/cards.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/bgimage.css';
+import '../styles/articles.css';
+import Layout from './layout/layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout >
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
