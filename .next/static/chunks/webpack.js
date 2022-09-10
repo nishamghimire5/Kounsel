d7d9528baaed2d0ad7c06b4e0d62cdfc4968775a
Prekshya Dawadi <prekshya019@gmail.com>
2022-09-10 03:34:40 +0000

    Homepage redesign 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0229c4e741f29d87"; }
+/******/ 		__webpack_require__.h = function() { return "95158e680afb6890"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/carddata.js
+++ pages/carddata.js
@@ -1,19 +1,30 @@
 const CardData = (props) => {
   // let height = { "mix-height": "390px" };
   return (
-    <div className="cards-list">
-      <a href="#">
-        <div className="card 1">
-          <img
-            className="card_image"
-            src={props.imgLink}
-            alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?"
-          />
-          <div className="card-title title-white">
-            <h4>{props.textInPic}</h4>
+    <div className="slide-container swiper">
+      <div className="slide-content">
+        <div class="card-wrapper swiper-wrapper">
+          <div class="card swiper-slide">
+            <div class="image-content">
+              <span class="overlay"></span>
+
+              <div class="card-image">
+                <img src={props.imgLink} alt="" className="card-img" />
+              </div>
+            </div>
+
+            <div class="card-content">
+              <h2 class="name">{props.textInPic}</h2>
+              <button class="button">View More</button>
+            </div>
+          </div>
+          <div class="card swiper-slide">
+            <div class="image-content">
+              <span class="overlay"></span>
+            </div>
           </div>
         </div>
-      </a>
+      </div>
     </div>
   );
 };
