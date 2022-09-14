4d241af9774061b5a613b9b6acaa3a304c986fa0
aayush125 <aayushbhatt54@gmail.com>
2022-09-14 22:23:16 +0545

    fancy pages

--- pages/layout/titlebar.js
+++ pages/layout/titlebar.js
@@ -40,62 +40,17 @@ const TitleBar = () => {
                         </Link>}
                         {status === "authenticated" &&
                         <Link href="">
-                            <a className='text-decoration-none text-dark ml-4 mr-4 fs-6' href="" onClick={()=> signOut()}>Logout {session.user.name}</a>
+                            <a className='text-decoration-none text-dark ml-4 mr-4 fs-6' href="" onClick={()=> signOut({callbackUrl: '/'})}>Logout {session.user.name}</a>
                         </Link>}
                         {status === "unauthenticated" &&
                         <Link href="">
                             <a className='text-decoration-none text-dark mr-4 fs-6' href="" onClick={()=> signIn('google', {callbackUrl: '/dashboard'})}>Login</a>
                         </Link>}
-                        {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Articles</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
-                        </NavDropdown> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
     </div>
     )
-    //     return (
-    //         <>
-    //             <nav className="navbar navbar-expand-lg navbar-light bg-light p-3">
-    //                 <Link className="navbar-brand" href="/">
-    //                     <a href="">Kounsel</a>
-    //                 </Link>
-    //                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    //                     <span className="navbar-toggler-icon"></span>
-    //                 </button>
-    //                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    //                     <ul className="navbar-nav">
-    //                         <li className="nav-item active">
-    //                             <Link className="nav-link" href="/"><a href="">Home</a> <span className="sr-only"></span></Link>
-    //                         </li>
-    //                         <li className="nav-item">
-    //                             <Link className="nav-link" href="/about"><a href="">About</a></Link>
-    //                         </li>
-    //                         <li className="nav-item">
-    //                             <Link className="nav-link" href="/articlesshow"><a href="">Articles</a></Link>
-    //                         </li>
-    //                         <li className="nav-item dropdown">
-    //                             <Link className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><a href="">Login</a>
-    //                             </Link>
-    //                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-    //                                 <Link className="dropdown-item" href='/signin'><a href="">Sign In</a></Link>
-    //                                 <Link className="dropdown-item" href='/singup'><a href="">Sign Up</a></Link>
-    //                                 {/* <div className="dropdown-divider"></div>
-    //                                 <Link className="dropdown-item" href='/'>Something else here</Link> */}
-    //                             </div>
-    //                         </li>
-    //                     </ul>
-    //                 </div>
-    //             </nav>
-    //         </>
-    //     )
 }
 export default TitleBar;
--- pages/unauthorized.jsx
+++ pages/unauthorized.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
 import {useSession, signIn, signOut} from 'next-auth/react'
 
 const failedauth = () => {
-    return(<div>Unauthorized: only @student.ku.edu.np or @ku.edu.np domains allowed.</div>)
+    return (
+        <>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <div className="text-center">
+                <h1 className="display-1 fw-bold">Unauthorized</h1>
+                <p className="fs-3">Only domains hosted at ku.edu.np are allowed.</p>
+                <p className="lead">
+                     Please try logging in again with a ku.edu.np domain.
+                </p>
+                <Link href="">
+                    <a href="" className="btn btn-light" onClick={()=> signIn('google', {callbackUrl: '/dashboard'})}>Login</a>
+                </Link>
+            </div>
+        </div>
+        </>
+    )
 }
 
 export default failedauth
