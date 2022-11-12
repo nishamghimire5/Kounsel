7b01f95070e8d4244e90d2e8eb0390ec569d92a5
Prekshya Dawadi <prekshya019@gmail.com>
2022-11-12 14:40:16 +0000

    navbar made responsive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ff2d47c81a15594"; }
+/******/ 		__webpack_require__.h = function() { return "d91c14e57c33350e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/layout/titlebar.js
+++ pages/layout/titlebar.js
@@ -1,9 +1,55 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const TitleBar = () => {
-    return (
-        <>
-            <header>
+  return (
+    <>
+      <header>      
+      <nav class="navbar navbar-expand-lg  nav-container">
+        <a class="navbar-brand nav-logo" href="/">
+          Kounsel
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav" style={{position: 'relative', left:'47%'}}>
+            <ul className="nav__links">
+            <Link class="nav-item nav-link active" href="/">
+              <li>Home <span class="sr-only"></span></li>
+            </Link>
+            <Link class="nav-item nav-link" href="/about">
+              <li>About</li>
+            </Link>
+            <Link class="nav-item nav-link" href="articles">
+              <li>Articles</li>
+            </Link>
+            <Link class="nav-item nav-link" href="/dashboard">
+              <li>Dashboard</li>
+            </Link>
+            <Link class="nav-item nav-link" href="/login">
+              <li>Login</li>
+            </Link>
+            <button className="cta-button">Get Started</button>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      </header>
+    </>
+  );
+};
+export default TitleBar;
+
+
+      {/* <header>
                 <nav>
                     <div className='nav-container'>
                         <div className='nav-logo'>
@@ -21,8 +67,4 @@ const TitleBar = () => {
                         </div>
                     </div>
                 </nav>
-            </header>
-        </>
-    )
-}
-export default TitleBar;
+            </header> */}
