2b8c2b7c71726c3c4763cbb0586a8afaf97df542
Prekshya Dawadi <prekshya019@gmail.com>
2022-11-13 19:34:16 +0000

    fix image size

--- pages/carddata.js
+++ pages/carddata.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 const CardData = (props) => {
     // let height = { "mix-height": "390px" };
     return (
-        <div className="col-sm-12 col-md-6 col-lg-4 mb-4 main"><div className="card text-white card-has-bg click-col" >
+        <div className="col-sm-12 col-md-12 col-lg-4 mb-4 main"><div className="card text-white card-has-bg click-col" >
             <div className="rounded-lg">
-                <Image className="card-img" src={props.image} alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?" />
+                <Image className="card-img" src={props.image} alt="Goverment Lorem Ipsum Sit Amet Consectetur dipisi?" width={2000} height={2000} />
                 <div className="card-img-overlay d-flex flex-column">
                     <div className="card-body">
                         <h4 className="card-title mt-0"><div className="card-img text-center">{props.textInPic}</div></h4>
