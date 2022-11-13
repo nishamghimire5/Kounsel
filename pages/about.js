3276cae0a826a987766c9575f67cac28f00d6521
Prekshya Dawadi <prekshya019@gmail.com>
2022-11-13 12:01:00 +0000

    About page made responsive

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,51 @@
+import Image from "next/image";
+import img from "../public/images/Students-rafiki.png";
+import FooterPage from "./layout/footer";
 const About = () => {
-    return (<><h2>About</h2></>)
-}
+  return (
+    <>
+      <div style={{ position: "absolute", left: "15%", top: "10rem" }}>
+        <h1 className="ms-5 ps-5 title">About Us</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm col-md col-lg">
+            <div class="left-aligned-img">
+              <Image
+                src={img}
+                width="500px"
+                height="500px"
+                layout="fixed"
+                alt="illustration of students"
+              />
+            </div>
+          </div>
+
+          <div
+            className="col-sm col-md col-lg"
+            style={{ position: "relative", top: "10rem" }}
+          >
+            <div class="body-text">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div style={{ position: "relative", top: "10rem" }}>
+        <FooterPage />
+      </div>
+    </>
+  );
+};
 
 export default About;
